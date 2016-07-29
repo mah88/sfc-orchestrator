d8e372d8ba7cb8472b217cfdb1a91259bfa5d7e9
Ahmed M. Medhat <ahmad.mohammed.medhat@fokus-extern.fraunhofer.de>
2016-07-29 10:54:49 +0200

    Deleted files manually

--- sfc-orch.sh
+++ sfc-orch.sh
@@ -5,7 +5,8 @@ source ./gradle.properties
 _version=${version}
 
 _project_base="/opt/openbaton/SFC"
-_process_name="SFC"
+_process_name="org-controller"
+_screen_subname="sfc-orchestrator"
 _screen_name="sfc-orch"
 _config_file="/etc/openbaton/SFC.properties"
 
@@ -48,9 +49,9 @@ function start {
     start_checks
     screen_exists=$(screen -ls | grep ${_screen_name} | wc -l);
     if [ "${screen_exists}" -eq 0 ]; then
-        screen -c screenrc -d -m -S ${_screen_name} -t ${_process_name} java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
+        screen -c screenrc -d -m -S ${_screen_name} -t ${_screen_subname} java -jar "build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
     else
-        screen -S $_screen_name -p 0 -X screen -t $_process_name java -jar "$_project_base/build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
+        screen -S $_screen_name -p 0 -X screen -t $_screen_subname java -jar "build/libs/$_process_name-$_version.jar" --spring.config.location=file:${_config_file}
     fi
 }
 
@@ -94,7 +95,7 @@ function end {
     exit
 }
 function usage {
-    echo -e "sfc-orchestrator\n"
+    echo -e "SFC\n"
     echo -e "Usage:\n\t ./sfc-orch.sh [compile|start|start_fg|test|kill|clean]"
 }
 
